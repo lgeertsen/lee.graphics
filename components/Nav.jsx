afcd41eb64b54c02b8ff233dd3bd7ad4ed2cc0ac
Lee Geertsen <lee.geertsen@hotmail.com>
2020-03-21 16:47:57 +0100

    Initial commit

---
+++ components/Nav.jsx
@@ -0,0 +1,270 @@
+import { useState } from 'react';
+
+const Nav = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+  <div className="navigation-wrapper">
+    <div className="left-navigation">
+      <a href="/" className="brand">
+      Lee  <span>G.</span>
+    </a>
+    <div className="hamburger-wrapper">
+      <div id="hamburger-square" className="" onClick={() => setOpen(!open)}>
+        <div className="hamburger-line"></div>
+        <div className="hamburger-line line-bottom"></div>
+      </div>
+    </div>
+
+    <div className="social-wrapper">
+      <div className="icons-wrapper">
+        <a href="https://www.linkedin.com/in/lgeertsen/" target="_blank" className="icon fa-stack">
+          <i className="fas fa-circle fa-stack-2x"></i>
+          <i className="fab fa-linkedin fa-stack-1x fa-inverse"></i>
+        </a>
+        <a href="https://github.com/lgeertsen" target="_blank" className="icon fa-stack">
+          <i className="fas fa-circle fa-stack-2x"></i>
+          <i className="fab fa-github fa-stack-1x fa-inverse"></i>
+        </a>
+      </div>
+    </div>
+  </div>
+
+  <div id="navigation-menu" className={open ? "open" : ""}>
+    <a href="index.html" className="nav-link">
+      <h1 className="nav-text">HOME</h1>
+      <div className="nav-line"></div>
+    </a>
+    <a href="Resume_GEERTSEN_Lee.pdf" className="nav-link" target="_blank">
+      <h1 className="nav-text">RESUME</h1>
+      <div className="nav-line"></div>
+    </a>
+    <a href="projects.html" className="nav-link">
+      <h1 className="nav-text">PROJECTS</h1>
+      <div className="nav-line"></div>
+    </a>
+    <a href="contact.html" className="nav-link">
+      <h1 className="nav-text">CONTACT</h1>
+      <div className="nav-line"></div>
+    </a>
+  </div>
+
+  <style jsx>{`
+    .navigation-wrapper {
+      position: fixed;
+      left: 0%;
+      top: 0%;
+      right: auto;
+      bottom: 0%;
+      z-index: 9998;
+    }
+
+    .left-navigation {
+      position: relative;
+      left: 0%;
+      top: 0%;
+      right: auto;
+      bottom: 0%;
+      z-index: 2;
+      display: -webkit-box;
+      display: -webkit-flex;
+      display: -ms-flexbox;
+      display: flex;
+      width: 90px;
+      height: 100%;
+      padding-top: 20px;
+      padding-bottom: 20px;
+      float: left;
+      -webkit-box-orient: vertical;
+      -webkit-box-direction: normal;
+      -webkit-flex-direction: column;
+      -ms-flex-direction: column;
+      flex-direction: column;
+      -webkit-box-pack: justify;
+      -webkit-justify-content: space-between;
+      -ms-flex-pack: justify;
+      justify-content: space-between;
+      -webkit-box-align: center;
+      -webkit-align-items: center;
+      -ms-flex-align: center;
+      align-items: center;
+      border-right: 1px solid #ededed;
+      background-color: #fff;
+    }
+
+    .brand {
+      position: relative;
+      z-index: 2;
+      display: -webkit-box;
+      display: -webkit-flex;
+      display: -ms-flexbox;
+      display: flex;
+      height: 90px;
+      width: 200px;
+      margin-top: 40px;
+      -webkit-box-pack: center;
+      -webkit-justify-content: center;
+      -ms-flex-pack: center;
+      justify-content: center;
+      -webkit-box-align: center;
+      -webkit-align-items: center;
+      -ms-flex-align: center;
+      align-items: center;
+      text-decoration: none;
+      color: #dfa25f;
+      font-family: 'Tuesday Night', 'Fjalla One', sans-serif;
+      padding-top: 15px;
+      font-size: 44px;
+      transform: rotate(-90deg);
+    }
+
+    .brand span {
+      padding-left: 30px;
+    }
+
+    .hamburger-wrapper {
+      display: -webkit-box;
+      display: -webkit-flex;
+      display: -ms-flexbox;
+      display: flex;
+      -webkit-box-pack: center;
+      -webkit-justify-content: center;
+      -ms-flex-pack: center;
+      justify-content: center;
+      -webkit-box-align: center;
+      -webkit-align-items: center;
+      -ms-flex-align: center;
+      align-items: center;
+      -webkit-box-flex: 0;
+      -webkit-flex: 0 auto;
+      -ms-flex: 0 auto;
+      flex: 0 auto;
+    }
+
+    #hamburger-square {
+      position: relative;
+      top: 0px;
+      right: 0px;
+      display: -webkit-box;
+      display: -webkit-flex;
+      display: -ms-flexbox;
+      display: flex;
+      width: 50px;
+      height: 50px;
+      margin-top: 0px;
+      -webkit-box-orient: vertical;
+      -webkit-box-direction: normal;
+      -webkit-flex-direction: column;
+      -ms-flex-direction: column;
+      flex-direction: column;
+      -webkit-box-pack: center;
+      -webkit-justify-content: center;
+      -ms-flex-pack: center;
+      justify-content: center;
+      -webkit-box-align: center;
+      -webkit-align-items: center;
+      -ms-flex-align: center;
+      align-items: center;
+      background-color: transparent;
+      cursor: pointer;
+    }
+
+    #hamburger-square .hamburger-line {
+      position: absolute;
+      width: 60%;
+      height: 1px;
+      background-color: #333;
+      transform-style: preserve-3d;
+      transform: translate3d(0px, -6px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg);
+      transition: 0.4s all ease;
+    }
+
+    #hamburger-square .hamburger-line.line-bottom {
+      transform: translate3d(0px, 6px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg);
+    }
+
+    .icon {
+      display: block;
+      margin-bottom: 10px;
+      font-size: 18px;
+      color: #dfa25f;
+      transition: 0.2s;
+    }
+
+    .icon:hover {
+      color: #d99244;
+    }
+
+    #navigation-menu {
+      position: relative;
+      left: auto;
+      top: 0%;
+      right: 400px;
+      bottom: auto;
+      z-index: 1;
+      display: -webkit-box;
+      display: -webkit-flex;
+      display: -ms-flexbox;
+      display: flex;
+      height: 100%;
+      width: 400px;
+      margin-left: 90px;
+      padding: 30px 180px 30px 90px;
+      -webkit-box-orient: vertical;
+      -webkit-box-direction: normal;
+      -webkit-flex-direction: column;
+      -ms-flex-direction: column;
+      flex-direction: column;
+      -webkit-box-pack: center;
+      -webkit-justify-content: center;
+      -ms-flex-pack: center;
+      justify-content: center;
+      -webkit-box-align: start;
+      -webkit-align-items: flex-start;
+      -ms-flex-align: start;
+      align-items: flex-start;
+      border-right: 1px solid #ededed;
+      background-color: #fff;
+      transition: 0.5s all ease;
+    }
+
+    #navigation-menu.open {
+      right: 0%;
+    }
+
+    .nav-link {
+      position: relative;
+      text-decoration: none;
+      color: #333;
+      transition: 0.3s all ease;
+    }
+
+    .nav-link:hover {
+      color: #555;
+    }
+
+    .nav-text {
+      position: relative;
+      z-index: 3;
+      font-family: 'Oswald', sans-serif;
+      font-weight: 400;
+    }
+
+    .nav-line {
+      position: absolute;
+      z-index: 2;
+      bottom: 12px;
+      height: 12px;
+      width: 0%;
+      background-color: #dfa25f;
+      transition: 0.3s all ease;
+    }
+
+    .nav-link:hover .nav-line {
+      width: 110%;
+    }
+  `}</style>
+</div>
+)}
+
+export default Nav
--- pages/index.js
+++ pages/index.js
@@ -1,203 +1,334 @@
 import Head from 'next/head'
 
+import Nav from '../components/Nav'
+
 const Home = () => (
-  <div className="container">
+  <React.Fragment>
     <Head>
-      <title>Create Next App</title>
+      <title>Lee Geertsen</title>
       <link rel="icon" href="/favicon.ico" />
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossOrigin="anonymous"/>
+      <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Oswald:300,400,700%7CLato:100,100italic,300,300italic,400,400italic,700,700italic,900,900italic%7CFjalla+One:regular%7CAverage:regular"/>
     </Head>
 
-    <main>
-      <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
+    <Nav/>
 
-      <p className="description">
-        Get started by editing <code>pages/index.js</code>
-      </p>
+    <div className="page-wrapper">
+      <section className="full-section home-section" id="home"></section>
 
-      <div className="grid">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+      <h1 id="top-title"><span className="tuesday-night">Lee <span className="offset">Geertsen</span></span><br/>Pipeline TD</h1>
 
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
+      <section className="full-page" id="hello">
+        <div className="full-page w-container wide-container">
+          <p className="half-p">I'm a last year VFX &amp; Game Programming student at <a href="https://artfx.school/en/technical-director-visual-effetcs-video-games-programming/" target="_blank" className="stylish-link">ArtFx</a> with a <em>passion for coding &amp; cocktails</em></p>
+        </div>
+      </section>
 
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
+      <section className="main-content">
 
-        <a
-          href="https://zeit.co/new?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>
-            Instantly deploy your Next.js site to a public URL with ZEIT Now.
-          </p>
-        </a>
-      </div>
-    </main>
+        <section className="full-section" id="demo-reel">
+          <div className="frame"></div>
+          <h1>Projects</h1>
+          <div className="projects-container">
+            <div className="project-card">
+              {/* <div className="frame frame-yellow"></div> */}
+              <div className="project-card-image"></div>
+              <div className="project-card-info">
+                <div className="project-card-content">
+                  <h2 className="project-title">Pulsar</h2>
+                  <div className="divider"></div>
+                  <p className="grey-text">Open source Pipeline</p>
+                </div>
+                <div className="frame frame-grey"></div>
+              </div>
+            </div>
 
-    <footer>
-      <a
-        href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
-      </a>
-    </footer>
+            <div className="project-card">
+              {/* <div className="frame frame-yellow"></div> */}
+              <div className="project-card-image"></div>
+              <div className="project-card-info">
+                <div className="project-card-content">
+                  <h2 className="project-title">Vyewer</h2>
+                  <div className="divider"></div>
+                  <p className="grey-text">Open source Pipeline</p>
+                </div>
+                <div className="frame frame-grey"></div>
+              </div>
+            </div>
 
-    <style jsx>{`
-      .container {
-        min-height: 100vh;
-        padding: 0 0.5rem;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
+            <div className="project-card">
+              {/* <div className="frame frame-yellow"></div> */}
+              <div className="project-card-image"></div>
+              <div className="project-card-info">
+                <div className="project-card-content">
+                  <h2 className="project-title">Magnetar</h2>
+                  <div className="divider"></div>
+                  <p className="grey-text">Open source Pipeline</p>
+                </div>
+                <div className="frame frame-grey"></div>
+              </div>
+            </div>
+          </div>
+        </section>
 
-      main {
-        padding: 5rem 0;
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
 
-      footer {
-        width: 100%;
-        height: 100px;
-        border-top: 1px solid #eaeaea;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
 
-      footer img {
-        margin-left: 0.5rem;
-      }
-
-      footer a {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      a {
-        color: inherit;
-        text-decoration: none;
-      }
-
-      .title a {
-        color: #0070f3;
-        text-decoration: none;
-      }
-
-      .title a:hover,
-      .title a:focus,
-      .title a:active {
-        text-decoration: underline;
-      }
-
-      .title {
-        margin: 0;
-        line-height: 1.15;
-        font-size: 4rem;
-      }
-
-      .title,
-      .description {
-        text-align: center;
-      }
-
-      .description {
-        line-height: 1.5;
-        font-size: 1.5rem;
-      }
-
-      code {
-        background: #fafafa;
-        border-radius: 5px;
-        padding: 0.75rem;
-        font-size: 1.1rem;
-        font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-          DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-      }
-
-      .grid {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-wrap: wrap;
-
-        max-width: 800px;
-        margin-top: 3rem;
-      }
-
-      .card {
-        margin: 1rem;
-        flex-basis: 45%;
-        padding: 1.5rem;
-        text-align: left;
-        color: inherit;
-        text-decoration: none;
-        border: 1px solid #eaeaea;
-        border-radius: 10px;
-        transition: color 0.15s ease, border-color 0.15s ease;
-      }
-
-      .card:hover,
-      .card:focus,
-      .card:active {
-        color: #0070f3;
-        border-color: #0070f3;
-      }
-
-      .card h3 {
-        margin: 0 0 1rem 0;
-        font-size: 1.5rem;
-      }
-
-      .card p {
-        margin: 0;
-        font-size: 1.25rem;
-        line-height: 1.5;
-      }
-
-      @media (max-width: 600px) {
-        .grid {
-          width: 100%;
-          flex-direction: column;
-        }
-      }
-    `}</style>
+      </section>
+    </div>
 
     <style jsx global>{`
+      @font-face {
+        font-family: "Tuesday Night";
+        src: url("/TuesdayNight/TuesdayNight-Regular.otf");
+      }
+
       html,
-      body {
+      body,
+      #__next,
+      body-container {
+        height: 100%;
         padding: 0;
         margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+        font-family: Average, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
           Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+        color: #333352;
+        font-size: 16px;
+        line-height: 22px;
       }
 
       * {
         box-sizing: border-box;
       }
+
+      ::selection {
+        background: #dfa25f;
+        color: #fff;
+      }
+
+      .page-wrapper {
+        padding-left: 90px;
+        height: 100%;
+      }
+
+      .full-section {
+        position: relative;
+        height: 100%;
+        background-color: #fff;
+      }
+
+      .full-page {
+        position: relative;
+        overflow: hidden;
+        height: 100%;
+      }
+
+      .w-container {
+        margin-left: auto;
+        margin-right: auto;
+        max-width: 940px;
+      }
+
+      .wide-container {
+        position: relative;
+        display: block;
+        max-width: 1224px;
+        margin-right: auto;
+        margin-left: auto;
+      }
+
+      .half-p {
+        position: absolute;
+        top: 24%;
+        width: 45%;
+        margin-left: -10px;
+        padding-bottom: 10px;
+        padding-left: 10px;
+        color: #d1d1d1;
+        font-size: 46px;
+        line-height: 58px;
+        text-shadow: 1px 1px 0 #fff;
+      }
+
+      .stylish-link {
+        color: #a4a4a4;
+        text-decoration: underline;
+        text-decoration-style: dotted;
+        font-weight: 800;
+      }
+
+      .frame {
+        position: absolute;
+        width: calc(100% - 20px);
+        height: calc(100% - 20px);
+        left: 10px;
+        top: 10px;
+        border: 1px solid #fff;
+        color: transparent;
+      }
+
+      .frame-grey {
+        border-color: #e3e3e3;
+      }
+
+      .frame.frame-yellow {
+        border-color: #dfa25f;
+      }
+
+      .divider {
+        width: 90px;
+        height: 4px;
+        margin-top: 8px;
+        margin-bottom: 8px;
+        border-top: 1px solid #dfa25f;
+        border-bottom: 1px solid #dfa25f;
+      }
+
+      .grey-text {
+        color: #a4a4a4;
+      }
     `}</style>
-  </div>
+
+    <style jsx>{`
+      .home-section {
+        border: 10px solid #fff;
+        overflow: hidden;
+        height: 67%;
+        background-color: rgb(233, 196, 186);
+        background-image: linear-gradient(180deg, rgba(255, 255, 255, .5), rgba(255, 255, 255, .5)), url("/denise-jans-ScQXxiIZ0Qo-unsplash.jpg");
+        // background-image: linear-gradient(180deg, rgba(255, 255, 255, .5), rgba(255, 255, 255, .5)), url("/patrick-ryan-703015-unsplash.jpg");
+        background-position: 50% 100%;
+        // background-position: 50% 0px;
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+      }
+
+      #top-title {
+        position: fixed;
+        right: 13.5%;
+        bottom: 28.5%;
+        display: inline-block;
+        margin-top: 0px;
+        margin-bottom: 0px;
+        font-family: 'Fjalla One', sans-serif;
+        font-size: 120px;
+        line-height: 150px;
+        font-weight: 700;
+        /* font-weight: 400; */
+        text-align: left;
+        text-transform: uppercase;
+        color: #333;
+      }
+
+      #top-title .tuesday-night {
+        font-family: 'Tuesday Night', 'Fjalla One', sans-serif;
+        text-transform: none;
+        font-weight: 400;
+        color: #dfa25f;
+        font-size: 120px;
+        text-shadow: 1px 1px 2px #444;
+      }
+
+      #top-title .offset {
+        margin-left: 50px;
+      }
+
+      .main-content {
+        position: relative;
+        z-index: 1;
+        height: 100%;
+      }
+
+      #demo-reel {
+        /* border: 10px solid #fff; */
+        background-color: #eee;
+        background-image: linear-gradient(180deg, rgba(0, 0, 0, .3), rgba(0, 0, 0, .3)), url("https://images.unsplash.com/photo-1532264523420-881a47db012d");
+        // background-image: linear-gradient(180deg, rgba(255, 255, 255, .3), rgba(255, 255, 255, .3)), url("https://images.unsplash.com/photo-1488229297570-58520851e868");
+        background-position: 50% 100%;
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+      }
+
+      #demo-reel h1 {
+        padding-top: 50px;
+        margin-bottom: 0px;
+        font-family: 'Fjalla One', sans-serif;
+        font-size: 120px;
+        line-height: 130px;
+        /* font-weight: 700; */
+        font-weight: 400;
+        text-align: center;
+        text-transform: uppercase;
+        color: #fff;
+      }
+
+      .projects-container {
+        display: flex;
+        flex-direction: row;
+        margin: 0 50px;
+        margin-top: 20px;
+        flex-wrap: wrap;
+        justify-content: space-around;
+      }
+
+      .project-card {
+        width: 600px;
+        // max-width: 600px;
+        height: 30vh;
+        position: relative;
+        display: flex;
+        flex-direction: row;
+        margin-bottom: 40px;
+        background-color: #fff;
+        box-shadow: 0 1px 2px 0 rgba(0, 0, 0, .12);
+      }
+
+      .project-card-image {
+        display: flex;
+        margin-top: 10px;
+        margin-left: 10px;
+        margin-bottom: 10px;
+        justify-content: center;
+        align-items: center;
+        flex: 1;
+        background-color: #e3e3e3;
+        background-image: url("https://images.unsplash.com/photo-1535016120720-40c646be5580");
+        background-position: 50% 50%;
+        background-size: cover;
+        background-repeat: no-repeat;
+        text-decoration: none;
+      }
+
+      .project-card-info {
+        position: relative;
+        display: flex;
+        width: 55%;
+        padding: 48px 20px;
+        justify-content: center;
+        align-items: center;
+      }
+
+      .project-card-content {
+        position: relative;
+        z-index: 1;
+        display: flex;
+        width: 100%;
+        padding-top: 8px;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+      }
+
+      .project-title {
+        color: #444;
+        font-family: 'Tuesday Night', 'Fjalla One', sans-serif;
+        font-size: 68px;
+        font-weight: 400;
+      }
+    `}</style>
+  </React.Fragment>
 )
 
 export default Home
